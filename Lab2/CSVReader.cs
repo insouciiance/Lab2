fcd26b7a8247aad31ed1de2a57513ed62fcdcae4
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-02-15 02:08:13 +0200

    added CSVReader.cs with the default constructor

---
+++ Lab2/CSVReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class CSVReader
+    {
+        private StreamReader _reader;
+
+        public CSVReader(string fileName)
+        {
+            string extension = fileName.Split('.').Last();
+            if (extension != "csv")
+            {
+                throw new FileLoadException("The file extension was not .csv.");
+            }
+
+            _reader = new StreamReader(fileName);
+        }
+    }
+}
