0766955a9a52f8d69911220ed39c5b76a266fae1
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-02-15 02:15:59 +0200

    added WriteLineAsync(params object[]) to CSVWriter.cs

--- Lab2/CSVWriter.cs
+++ Lab2/CSVWriter.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Lab2
 {
-    class CSVWriter
+    internal class CSVWriter
     {
+        private StreamWriter _writer;
+
+        public CSVWriter(string fileName)
+        {
+            string extension = fileName.Split('.').Last();
+            if (extension != "csv")
+            {
+                throw new FileLoadException("The file extension was not .csv.");
+            }
+
+            _writer = new StreamWriter(fileName);
+        }
+
+        public async Task WriteLineAsync(params object[] values)
+        {
+            if (values == null)
+            {
+                return;
+            }
+
+            foreach (object value in values)
+            {
+                await _writer.WriteAsync(value?.ToString());
+            }
+
+            await _writer.WriteAsync(Environment.NewLine);
+        }
     }
 }
