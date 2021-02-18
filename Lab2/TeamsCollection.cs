88625d5df5685f4268562034bc856837c37e2fc4
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-02-19 00:16:50 +0200

    added TeamsCollection

---
+++ Lab2/TeamsCollection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab2
+{
+    public class TeamsCollection : IEnumerable<Team>
+    {
+        private List<Team> _teams;
+
+        public TeamsCollection()
+        {
+            _teams = new List<Team>();
+        }
+
+        public TeamsCollection(Team[] teams)
+        {
+            _teams = new List<Team>(teams);
+        }
+
+        public Team[] GetTeamsRating() => _teams.OrderBy(team => team.Rating).ToArray();
+
+        public void Add(Team team) => _teams.Add(team);
+
+        public IEnumerator<Team> GetEnumerator() => _teams.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
