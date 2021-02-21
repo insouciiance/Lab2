ffb870269c9192e2e0e4629fd929820d526cade4
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-02-21 16:35:38 +0200

    made Team implement IEquatable<Team>

--- Lab2/Team.cs
+++ Lab2/Team.cs
@@ -2,10 +2,10 @@
 
 namespace Lab2
 {
-    public class Team : IComparable<Team>
+    public class Team : IComparable<Team>, IEquatable<Team>
     {
         public string Name { get; private set; }
-        public int Rating { get; private set; }
+        public int Rating { get; set; }
 
         public Team(string name, int rating)
         {
@@ -14,6 +14,8 @@ namespace Lab2
         }
 
         public int CompareTo(Team other) => other?.Rating - this.Rating ?? -1;
+
+        public bool Equals(Team another) => this.Name == another?.Name;
 
         public override string ToString()
         {
