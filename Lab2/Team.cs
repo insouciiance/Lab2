507b801a30c41daf119c7e5507ab3811ca744282
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-02-24 08:44:25 +0200

    added Scored and Missed to Team.cs

--- Lab2/Team.cs
+++ Lab2/Team.cs
@@ -6,11 +6,20 @@ namespace Lab2
     {
         public string Name { get; private set; }
         public int Rating { get; set; }
+    
+        public int Scored { get; set; }
+        public int Missed { get; set; }
 
         public Team(string name, int rating)
         {
             this.Name = name;
             this.Rating = rating;
+        }
+
+        public Team(string name, int rating, int scored, int missed) : this(name, rating)
+        {
+            Scored = scored;
+            Missed = missed;
         }
 
         public int CompareTo(Team other) => other?.Rating - this.Rating ?? -1;
